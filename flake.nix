d66cfc80fce1724276acd305eb53831fd1b07d0f
Caroline Larimore <caroline@larimo.re>
2025-04-21 18:04:30 -0700

    Create nix package and overlay

--- flake.nix
+++ flake.nix
@@ -2,19 +2,32 @@
   inputs = rec {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
-  outputs = inputs @ {
-    self,
-    nixpkgs,
-    ...
-  }:
-  let
+
+  outputs = inputs@{self, nixpkgs, ... }: let
+    system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
-  in
-  {
-    devShells.x86_64-linux.default = pkgs.mkShell {
+  in {
+    devShells.${system}.default = pkgs.mkShell {
       packages = with pkgs; [
         go
       ];
+    };
+
+    packages.${system} = {
+      default = self.packages.${system}.corvid;
+      corvid = pkgs.buildGoModule {
+        pname = "corvid";
+        version = "v1.0.0";
+
+        src = ./.;
+
+        vendorHash = "sha256-WUTGAYigUjuZLHO1YpVhFSWpvULDZfGMfOXZQqVYAfs=";
+      };
+    };
+
+    overlays.default = self.overlays.corvid;
+    overlays.corvid = final: prev: {
+      corvid = self.packages.${system}.corvid;
     };
   };
 }
